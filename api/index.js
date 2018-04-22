add31a8c5037f5dc94b8f6823162b5ca8bd8c88c
Razborges <razborges@me.com>
2018-04-22 19:07:26 -0300

    Register ager

--- api/index.js
+++ api/index.js
@@ -4,60 +4,46 @@ const axios = require('axios');
 const URL = process.env.API_URLBASE;
 
 // ROBOT
-const getRobot = (numberSeries) => {
-  axios.get(`${URL}robot/${numberSeries}`)
-    .then(response => response.data)
-    .catch(error => error);
-};
+const getRobot = numberSeries => axios.get(`${URL}robot/${numberSeries}`)
+  .then(response => response.data)
+  .catch(error => error);
 
 // USER
-const getUser = (serviceId) => {
-  axios.get(`${URL}user/${serviceId}`)
-    .then(response => response.data)
-    .catch(error => error);
-};
+const getUser = serviceId => axios.get(`${URL}user/${serviceId}`)
+  .then(response => response.data)
+  .catch(error => error);
 
-const addUser = (numberSeries, user) => {
-  axios.post(`${URL}user/${numberSeries}`, {
-    name: user.name,
-    email: user.email,
-    service: user.service,
-    serviceId: user.serviceId,
-  })
-    .then(response => response.data)
-    .catch(error => error);
-};
+const addUser = (numberSeries, user) => axios.post(`${URL}user/${numberSeries}`, {
+  name: user.name,
+  email: user.email,
+  service: user.service,
+  serviceId: user.serviceId,
+})
+  .then(response => response.data)
+  .catch(error => error);
 
 // ROUTE
-const getRoute = (numberSeries) => {
-  axios.get(`${URL}route/${numberSeries}`)
-    .then(response => response.data)
-    .catch(error => error);
-};
+const getRoute = numberSeries => axios.get(`${URL}route/${numberSeries}`)
+  .then(response => response.data)
+  .catch(error => error);
 
-const addRoute = (numberSeries, route) => {
-  axios.post(`${URL}route/${numberSeries}`, {
-    name: route.name,
-    type: route.type,
-    start: route.start,
-  })
-    .then(response => response.data)
-    .catch(error => error);
-};
+const addRoute = (numberSeries, route) => axios.post(`${URL}route/${numberSeries}`, {
+  name: route.name,
+  type: route.type,
+  start: route.start,
+})
+  .then(response => response.data)
+  .catch(error => error);
 
 // WORK
-const getWork = (idRoute) => {
-  axios.get(`${URL}work/${idRoute}`)
-    .then(response => response.data)
-    .catch(error => error);
-};
+const getWork = idRoute => axios.get(`${URL}work/${idRoute}`)
+  .then(response => response.data)
+  .catch(error => error);
 
 // BATTERY
-const getBattery = (numberSeries) => {
-  axios.get(`${URL}battery/${numberSeries}`)
-    .then(response => response.data)
-    .catch(error => error);
-};
+const getBattery = numberSeries => axios.get(`${URL}battery/${numberSeries}`)
+  .then(response => response.data)
+  .catch(error => error);
 
 const API = {
   getRobot,
--- index.js
+++ index.js
@@ -1,35 +1,90 @@
 require('dotenv').config();
 const Api = require('./api');
+const User = require('./models/User');
 const TelegramBot = require('node-telegram-bot-api');
 
-// replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_TOKEN;
+let bot = '';
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true });
-// const bot = new TelegramBot(token, { polling: true, webHook: { port: process.env.PORT } });
+if (process.env.DEV) {
+  bot = new TelegramBot(token, { polling: true });
+} else {
+  bot = new TelegramBot(token, { polling: true, webHook: { port: process.env.PORT } });
+  bot.setWebHook(`${process.env.URL}:${process.env.PORT}/${token}`);
+}
 
-// bot.setWebHook(`${process.env.URL}:${process.env.PORT}/${token}`);
-
-// Matches "/echo [whatever]"
-bot.onText(/\/start/, (info) => {
+// COMMAND /start
+bot.onText(/\/start/, async (info) => {
   const chatId = info.chat.id;
-  const name = info.from.username;
+  const name = `@${info.from.username}`;
+  const user = await Api.getUser(info.from.id);
 
-  const message = `Olá ${name}! Eu sou o AgerBot, vamos começar?`;
-  const user = Api.getUser(info.from.id);
+  if (!user.data.result) {
+    const message1 = `Olá ${name}! Eu sou o *AgerBot*. Eu irei te auxiliar durante a nossa conversa.`;
 
-  console.log('USER', user);
+    const message2 = 'Vi que você é um usuário que ainda não possui um *AGER*. O *AGER* é um robô agrícola, e eu posso lhe ajudar a verificar algumas informações coletadas por ele.';
 
-  bot.sendMessage(chatId, message);
+    await bot.sendMessage(chatId, message1, { parse_mode: 'markdown' });
+    await bot.sendMessage(chatId, message2, { parse_mode: 'markdown' });
+
+    await bot.sendMessage(chatId, 'Vamos começar? Escollha uma das opções abaixo:', {
+      reply_markup: {
+        one_time_keyboard: true,
+        keyboard: [['Registrar um AGER'], ['Saber mais sobre o AGER']],
+      },
+    });
+  }
+
+  if (user.data.result) {
+    const message3 = `${name} estou pronto para lhe ajudar novamente!`;
+    const message4 = 'Escolha uma das opções do menu, e não se esqueça: a qualquer momento você pode utilizar nossos comandos especiais: `/start`';
+
+    await bot.sendMessage(chatId, message3, { parse_mode: 'markdown' });
+    await bot.sendMessage(chatId, message4, {
+      parse_mode: 'markdown',
+      reply_markup: {
+        one_time_keyboard: true,
+        keyboard: [['Ver minhas rotas'], ['Verificar temperaturas'], ['Verificar humidade'], ['Nível da bateria']],
+      },
+    });
+  }
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  console.log(msg);
-  const chatId = msg.chat.id;
+// REGISTER A AGER
+bot.onText(/Registrar um AGER/, async (info) => {
+  const chatId = info.chat.id;
+  const name = `${info.from.first_name} ${info.from.last_name}`;
+  const service = 'telegram';
+  const serviceId = info.from.id;
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  bot.on('contact', async (phone) => { console.log(phone); });
+
+  await bot.sendMessage(chatId, 'Para registrar um *AGER* você deve nos informar o número de série e seu e-mail separados por hífen. Exemplo: _12345abc - seu@email.com_', {
+    parse_mode: 'markdown',
+    reply_markup: {
+      remove_keyboard: true,
+    },
+  });
+  await bot.sendMessage(chatId, 'Está pronto? Digite agora:', {
+    parse_mode: 'markdown',
+  });
+
+  bot.on('message', async (numberMsg) => {
+    const [numberSeries, email] = numberMsg.text.toString().split(' - ');
+
+    if (numberSeries && email) {
+      const user = new User(name, email, service, serviceId);
+      const result = await Api.addUser(numberSeries, user);
+
+      console.log('RESULT', result);
+    } else {
+      bot.sendMessage(chatId, 'As informações não estão corretas, tente novamente.', {
+        parse_mode: 'markdown',
+        reply_markup: {
+          one_time_keyboard: true,
+          keyboard: [['Registrar um AGER'], ['Saber mais sobre o AGER']],
+        },
+      });
+    }
+  });
 });
