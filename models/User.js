71252efc8ca51b7e4d37115e956440f1a61584ef
Razborges <razborges@me.com>
2018-04-22 19:07:10 -0300

    User model

---
+++ models/User.js
@@ -0,0 +1,10 @@
+class User {
+  constructor(name, email, service, serviceId) {
+    this.name = name;
+    this.email = email;
+    this.service = service;
+    this.serviceId = serviceId;
+  }
+}
+
+module.exports = User;
